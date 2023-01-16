6fdb3dc84233e66f163baa0787432fb61a1cb72d
Lucas de Felice <lucasfelice92@gmail.com>
2023-01-16 15:35:59 -0300

    subindo alterações em commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,3 +12,33 @@ Cypress.Commands.add('postUser', (_url, nome, email, password, administrador) =>
       failOnStatusCode: false
     })
   })
+
+Cypress.Commands.add('getUser', (nome, email, _id) => {
+    cy.request({
+      method: 'GET',
+      url: '/usuarios',
+      headers: { 'content-type': 'application/json'},
+      // qs: {
+      //   nome: nome,
+      //   email: email,
+      //   _id: _id
+      // },
+      failOnStatusCode: false
+    })
+
+Cypress.Commands.add('getToken', (_url, user, password) => {
+    cy.request({
+      method: 'POST',
+      url: `${_url}`,
+      headers: { 'content-type': 'application/json' },
+      body: {
+        email: user,
+        senha: password
+      },
+      failOnStatusCode: false
+    }).its('body.token').should('not.be.empty')
+    .then(token =>{
+      return token
+    })
+})  
+  })
